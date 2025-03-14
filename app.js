e8cba1ddfab31b1429301ce78c9478ab1a91c030
Hamsandwich1 <Jteahan24@gmail.com>
2025-03-14 23:12:40 +0000

    Live demo change

--- app.js
+++ app.js
@@ -37,3 +37,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
